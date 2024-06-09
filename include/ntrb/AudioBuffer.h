26982fb4326e696f44e1e5ca61f3e0cc9c4e7db6
FriedHamCheese <160022244+FriedHamCheese@users.noreply.github.com>
2024-06-09 15:39:40 +0700

    c o p y r i g h t  n o t i c e

--- include/ntrb/AudioBuffer.h
+++ include/ntrb/AudioBuffer.h
@@ -2,6 +2,18 @@
 #define ntrb_AudioBuffer_h
 
 /**
+Copyright 2024 Pawikan Boonnaum
+
+This file is part of naturau-base/ntrb.
+
+naturau-base/ntrb is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+naturau-base/ntrb is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with naturau-base/ntrb. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/**
 \file AudioBuffer.h
 A module providing the ntrb_AudioBuffer: an abstracted audio buffer, 
 functions for creating and deleting the type, and enums for reporting errors relating to the type.
